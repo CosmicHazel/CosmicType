dc8c93481b97074baea44a7535e8d87dd108be13
CosmicHazel <7584038+CosmicHazel@users.noreply.github.com>
2024-08-29 23:24:17 -0700

    yay

--- app.js
+++ app.js
@@ -18,7 +18,7 @@
 
 // Load words from LocalStorage or initialize with default word list
 // This array contains a list of common English words
-let wordArray = ["able", "about", "above", "add", "after", "again", "air", "all", "almost", "along", "also", "always", "an", "and", "animal", "another", "answer", "any", "are", "around", "as", "ask", "at", "away", "back", "be", "because", "become", "been", "before", "began", "begin", "below", "between", "big", "book", "both", "boy", "bring", "build", "but", "button", "buy", "by", "cable", "call", "came", "can", "car", "carry", "change", "CharaChorder", "child", "children", "city", "close", "code", "come", "computer", "con", "consider", "could", "country", "course", "cut", "day", "develop", "did", "difference", "different", "do", "does", "don't", "down", "download", "each", "early", "earth", "eat", "EMG", "end", "engineer", "enough", "even", "ever", "every", "example", "eye", "face", "fact", "family", "far", "fast", "father", "feel", "feet", "few", "fill", "find", "fine", "fire", "first", "follow", "food", "for", "form", "found", "four", "from", "general", "get", "girl", "give", "go", "gone", "good", "got", "govern", "great", "group", "grow", "had", "hand", "happen", "hard", "has", "have", "Hazel", "he", "head", "hear", "hello", "help", "her", "here", "hi", "high", "him", "his", "hit", "hold", "home", "hope", "house", "how", "however", "Hulet", "I'll", "I'm", "idea", "if", "important", "in", "increase", "interest", "into", "is", "isn't", "issue", "it", "it's", "just", "keep", "kind", "know", "land", "large", "last", "late", "later", "lead", "learn", "leave", "left", "let", "letter", "life", "light", "like", "line", "list", "little", "live", "load", "long", "look", "lose", "lot", "made", "make", "man", "many", "may", "me", "mean", "men", "menu", "might", "mile", "mind", "miss", "more", "most", "mother", "mountain", "move", "much", "must", "my", "name", "nation", "near", "need", "never", "new", "next", "nice", "night", "no", "Nostrand", "not", "note", "nothing", "now", "number", "of", "off", "often", "oil", "old", "on", "once", "one", "only", "open", "or", "order", "other", "our", "out", "over", "own", "page", "paper", "part", "past", "people", "person", "picture", "place", "plan", "plant", "play", "point", "possible", "present", "print", "problem", "probably", "program", "public", "put", "question", "quick", "quickly", "quite", "read", "real", "really", "review", "right", "river", "run", "said", "same", "saw", "say", "school", "screen", "sea", "second", "see", "seem", "seen", "sentence", "set", "she", "should", "show", "side", "sin", "since", "site", "small", "so", "some", "something", "sometime", "song", "soon", "sound", "spell", "stand", "start", "state", "still", "stop", "store", "story", "study", "such", "super", "sure", "system", "take", "talk", "team", "tell", "than", "thank", "that", "the", "their", "them", "then", "there", "these", "they", "thing", "think", "this", "those", "though", "thought", "three", "through", "time", "tip", "to", "together", "too", "took", "top", "tree", "try", "turn", "two", "type", "under", "until", "up", "us", "use", "used", "very", "walk", "want", "was", "watch", "water", "way", "we", "week", "well", "went", "were", "what", "when", "where", "which", "while", "white", "who", "why", "wide", "will", "with", "without", "won't", "word", "work", "world", "would", "write", "year", "you", "young", "your"];
+let wordArray = ["able", "about", "above", "add", "after", "again", "air", "all", "almost", "along", "also", "always", "an", "and", "animal", "another", "answer", "any", "are", "around", "at", "away", "back", "be", "because", "become", "been", "before", "began", "begin", "below", "between", "big", "book", "both", "boy", "bring", "build", "but", "button", "buy", "by", "cable", "call", "came", "can", "car", "carry", "change", "CharaChorder", "child", "children", "city", "close", "code", "come", "computer", "con", "consider", "could", "country", "course", "cut", "day", "develop", "did", "difference", "different", "do", "does", "don't", "down", "download", "each", "early", "earth", "eat", "EMG", "end", "engineer", "enough", "even", "ever", "every", "example", "eye", "face", "fact", "family", "far", "fast", "father", "feel", "feet", "few", "fill", "find", "fine", "fire", "first", "follow", "food", "for", "form", "found", "four", "from", "general", "get", "girl", "give", "go", "gone", "good", "got", "govern", "great", "group", "grow", "had", "hand", "happen", "hard", "has", "have", "Hazel", "he", "head", "hear", "hello", "help", "her", "here", "hi", "high", "him", "his", "hit", "hold", "home", "hope", "house", "how", "however", "Hulet", "I'll", "I'm", "idea", "if", "important", "in", "increase", "interest", "into", "is", "isn't", "issue", "it", "it's", "just", "keep", "kind", "know", "land", "large", "last", "late", "later", "lead", "learn", "leave", "left", "let", "letter", "life", "light", "like", "line", "list", "little", "live", "load", "long", "look", "lose", "lot", "made", "make", "man", "many", "may", "me", "mean", "men", "menu", "might", "mile", "mind", "miss", "more", "most", "mother", "mountain", "move", "much", "must", "my", "name", "nation", "near", "need", "never", "new", "next", "nice", "night", "no", "Nostrand", "not", "note", "nothing", "now", "number", "of", "off", "often", "oil", "old", "on", "once", "one", "only", "open", "or", "order", "other", "our", "out", "over", "own", "page", "paper", "part", "past", "people", "person", "picture", "place", "plan", "plant", "play", "point", "possible", "present", "print", "problem", "probably", "program", "public", "put", "question", "quick", "quickly", "quite", "read", "real", "really", "review", "right", "river", "run", "said", "same", "saw", "say", "school", "screen", "sea", "second", "see", "seem", "seen", "sentence", "set", "she", "should", "show", "side", "sin", "since", "site", "small", "so", "some", "something", "sometime", "song", "soon", "sound", "spell", "stand", "start", "state", "still", "stop", "store", "story", "study", "such", "super", "sure", "system", "take", "talk", "team", "tell", "than", "thank", "that", "the", "their", "them", "then", "there", "these", "they", "thing", "think", "this", "those", "though", "thought", "three", "through", "time", "tip", "to", "together", "too", "took", "top", "tree", "try", "turn", "two", "type", "under", "until", "up", "us", "use", "used", "very", "walk", "want", "was", "watch", "water", "way", "we", "week", "well", "went", "were", "what", "when", "where", "which", "while", "white", "who", "why", "wide", "will", "with", "without", "won't", "word", "work", "world", "would", "write", "year", "you", "young", "your"];
 
 // Initialize an object to store word statistics
 let words = {};
@@ -42,9 +42,9 @@ let wordStart;  // Timestamp for when the current word started
 let currentWords = [];  // Array of words currently displayed
 let nextWords = [];  // Array of words to be displayed next
 let futureWords = [];  // Array of words to be displayed after nextWords
-let currentSlowWords = []; // Array to store the current slow words
 let lineIndex = 0; // Keep track of which line we're on
 let wordIndex = 0; // Keep track of which word we're on in the current line
+let selectedSlowWords = []; // New global variable to store the selected slow words
 
 /**
  * Function to display words on the screen
@@ -56,13 +56,13 @@ function displayWords() {
     wordsContainer.innerHTML = ''; // Clear the words container
     
     // Get the slowest words
-    currentSlowWords = getSlowestWords(slowWordsNum);
+    selectedSlowWords = getSlowestWords(slowWordsNum);
     
     // Generate three lines of words, each containing 8 words chosen from the 5 slow words
     let lines = [
-        getRandomWords(currentSlowWords, wordsPerLine),
-        getRandomWords(currentSlowWords, wordsPerLine),
-        getRandomWords(currentSlowWords, wordsPerLine)
+        getRandomWords(selectedSlowWords, wordsPerLine),
+        getRandomWords(selectedSlowWords, wordsPerLine),
+        getRandomWords(selectedSlowWords, wordsPerLine)
     ];
     
     // Set the current, next, and future words
@@ -205,37 +205,70 @@ function calculateWordStats() {
  * Function to get the slowest words
  * This function identifies the slowest words based on average WPM and typing frequency.
  * It ensures a mix of untyped and slow typed words.
+ * Words in selectedSlowWords are not replaced until they have been typed at least once.
  * @param {number} count - The number of slow words to return
  * @returns {Array} An array of the slowest words
  */
 function getSlowestWords(count) {
+    console.log(`Starting getSlowestWords function with count: ${count}`);
+    
+    // Calculate and sort word statistics
     let wordStats = calculateWordStats();
     wordStats.sort((a, b) => a.averageWPM - b.averageWPM);
+    console.log('Word stats calculated and sorted:', wordStats);
 
     let slowestWords = [];
+    // Separate untyped words (Infinity WPM) from typed words
     let untypedWords = wordStats.filter(w => w.averageWPM === Infinity).map(w => w.word);
     let typedWords = wordStats.filter(w => w.averageWPM !== Infinity).map(w => w.word);
+    console.log('Untyped words:', untypedWords);
+    console.log('Typed words:', typedWords);
 
-    // Fill with untyped words first
+    // Step 1: Include words from selectedSlowWords that haven't been typed yet
+    selectedSlowWords.forEach(word => {
+        if (untypedWords.includes(word) && slowestWords.length < count) {
+            slowestWords.push(word);
+            untypedWords = untypedWords.filter(w => w !== word);
+            console.log(`Added untyped selectedSlowWord: ${word}`);
+        }
+    });
+
+    // Step 2: Fill with other untyped words
     while (slowestWords.length < count && untypedWords.length > 0) {
         let randomIndex = Math.floor(Math.random() * untypedWords.length);
-        slowestWords.push(untypedWords[randomIndex]);
+        let selectedWord = untypedWords[randomIndex];
+        slowestWords.push(selectedWord);
         untypedWords.splice(randomIndex, 1);
+        console.log(`Added random untyped word: ${selectedWord}`);
     }
 
-    // Then fill with the slowest typed words
+    // Step 3: Fill with the slowest typed words
     while (slowestWords.length < count && typedWords.length > 0) {
-        slowestWords.push(typedWords.shift());
-    }
-
-    // If we still don't have enough words, add random words from the original wordArray
-    while (slowestWords.length < count) {
-        let randomWord = wordArray[Math.floor(Math.random() * wordArray.length)];
-        if (!slowestWords.includes(randomWord)) {
-            slowestWords.push(randomWord);
+        let nextWord = typedWords.shift();
+        // Include the word if it's not in selectedSlowWords or if it has been typed at least once
+        if (!selectedSlowWords.includes(nextWord) || wordStats.find(w => w.word === nextWord).count > 0) {
+            slowestWords.push(nextWord);
+            console.log(`Added slow typed word: ${nextWord}`);
+        } else {
+            console.log(`Skipped selectedSlowWord that hasn't been typed: ${nextWord}`);
         }
     }
 
+    // Step 4: If we still don't have enough words, add random words from the original wordArray
+    while (slowestWords.length < count) {
+        let randomWord = wordArray[Math.floor(Math.random() * wordArray.length)];
+        // Include the word if it's not already in slowestWords and
+        // if it's not in selectedSlowWords or if it has been typed at least once
+        if (!slowestWords.includes(randomWord) && 
+            (!selectedSlowWords.includes(randomWord) || wordStats.find(w => w.word === randomWord).count > 0)) {
+            slowestWords.push(randomWord);
+            console.log(`Added random word from wordArray: ${randomWord}`);
+        } else {
+            console.log(`Skipped random word: ${randomWord}`);
+        }
+    }
+
+    console.log('Final list of slowest words:', slowestWords);
     return slowestWords;
 }
 
@@ -248,8 +281,8 @@ function displayStats() {
     let stats = calculateWordStats();
     
     // Separate current slow words from other words
-    let slowWordStats = stats.filter(stat => currentSlowWords.includes(stat.word));
-    let otherWordStats = stats.filter(stat => !currentSlowWords.includes(stat.word));
+    let slowWordStats = stats.filter(stat => selectedSlowWords.includes(stat.word));
+    let otherWordStats = stats.filter(stat => !selectedSlowWords.includes(stat.word));
     
     // Sort slow words: never-typed first, then by averageWPM
     slowWordStats.sort((a, b) => {
@@ -314,7 +347,7 @@ function displayStats() {
 
     stats.forEach(({ word, total, averageWPM }) => {
         let row = document.createElement('tr');
-        if (currentSlowWords.includes(word)) {
+        if (selectedSlowWords.includes(word)) {
             row.classList.add('slow');
         }
         [
@@ -339,14 +372,15 @@ function displayStats() {
 
 function shiftWords() {
     // Update the current slow words
-    currentSlowWords = getSlowestWords(slowWordsNum);
+    selectedSlowWords = getSlowestWords(slowWordsNum);
 
     // Shift word arrays: current becomes next
     currentWords = nextWords;
+    
     // Move future words to next
     nextWords = futureWords;
     // Generate new future words from the updated slow words
-    futureWords = getRandomWords(currentSlowWords, wordsPerLine);
+    futureWords = getRandomWords(selectedSlowWords, wordsPerLine);
 }
 
 function updateWordDisplay() {
